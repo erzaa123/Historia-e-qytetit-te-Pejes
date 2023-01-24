93e07637495fc7a1ee88dd7f1e1a49c1a794b05b
erzaa123 <erza.haxhiajdini@gmail.com>
2023-01-24 19:48:23 +0100

    init

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<header>
+        <div class="main">
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="AboutUs.php">About Us</a></li> 
+                <li><a href="ContactUs.php">Contact Us</a></li>  
+                <li><a href="News.php">News</a></li>
+                <button class="btn"><a href="Login.html">LOGIN</a></button>
+            </ul>
+        </div>
+    </header>  
+</body>
+</html>
