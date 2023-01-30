24a1117cbc6175b9deaee5b0bc13c3fc47f5fd69
erzaa123 <erza.haxhiajdini@gmail.com>
2023-01-30 21:57:07 +0100

    init

--- index.php
+++ index.php
@@ -8,46 +8,18 @@
     <title>Document</title>
     
     <style>
-
-* {box-sizing: border-box}
-body {font-family: Verdana, sans-serif; margin:0}
-.mySlides {display: none}
+* {box-sizing: border-box;}
+body {font-family: Verdana, sans-serif;}
+.mySlides {display: none;}
 img {vertical-align: middle;}
- 
+
 /* Slideshow container */
 .slideshow-container {
   max-width: 1000px;
   position: relative;
   margin: auto;
 }
- 
-/* Next & previous buttons */
-.prev, .next {
-  cursor: pointer;
-  position: absolute;
-  top: 50%;
-  width: auto;
-  padding: 16px;
-  margin-top: -22px;
-  color: white;
-  font-weight: bold;
-  font-size: 18px;
-  transition: 0.6s ease;
-  border-radius: 0 3px 3px 0;
-  user-select: none;
-}
- 
-/* Position the "next button" to the right */
-.next {
-  right: 0;
-  border-radius: 3px 0 0 3px;
-}
- 
-/* On hover, add a black background color with a little bit see-through */
-.prev:hover, .next:hover {
-  background-color: rgba(0,0,0,0.8);
-}
- 
+
 /* Caption text */
 .text {
   color: #f2f2f2;
@@ -58,10 +30,18 @@ img {vertical-align: middle;}
   width: 100%;
   text-align: center;
 }
- 
+
+/* Number text (1/3 etc) */
+.numbertext {
+  color: #f2f2f2;
+  font-size: 12px;
+  padding: 8px 12px;
+  position: absolute;
+  top: 0;
+}
+
 /* The dots/bullets/indicators */
 .dot {
-  cursor: pointer;
   height: 15px;
   width: 15px;
   margin: 0 2px;
@@ -70,56 +50,28 @@ img {vertical-align: middle;}
   display: inline-block;
   transition: background-color 0.6s ease;
 }
- 
-.active, .dot:hover {
+
+.active {
   background-color: #717171;
 }
- 
+
 /* Fading animation */
 .fade {
   animation-name: fade;
   animation-duration: 1.5s;
 }
- 
+
 @keyframes fade {
   from {opacity: .4} 
   to {opacity: 1}
 }
- 
+
 /* On smaller screens, decrease text size */
 @media only screen and (max-width: 300px) {
-  .prev, .next,.text {font-size: 11px}
+  .text {font-size: 11px}
 }
 </style>
-<script>
-let slideIndex = 1;
-showSlides(slideIndex);
- 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
- 
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
- 
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-}
 
-</script>
  
 </head>
 <body>
@@ -136,47 +88,66 @@ function showSlides(n) {
     </header> 
     <br><br><br><br><br><br>
 
-<div class="slideshow-container">
- 
-        <div class="mySlides fade">
-          
-          <img src="foto1.jpg" style="width:100vh" height="300">
-          <div class="text">Caption Text</div>
-        </div>
- 
-        <div class="mySlides fade">
-        
-          <img src="foto2.jpg" style="width:100%" height="300">
-          <div class="text">Caption Two</div>
-        </div>
+    <div class="slideshow-container">
 
-
-        <div class="mySlides fade">
-          
-          <img src="foto3.jpg" style="width:100%" height="300">
-          <div class="text">Caption Two</div>
-        </div>
-
-        <div class="mySlides fade">
-          
-          <img src="foto4.jpg" style="width:100%" height="300">
-          <div class="text">Caption Two</div>
-        </div>
- 
-        <a class="prev" onclick="plusSlides(-1)">&lt;</a>
-        <a class="next" onclick="plusSlides(1)">&gt;</a>
-        <a class="next" onclick="plusSlides(1)">&gt;</a>
-        <a class="next" onclick="plusSlides(1)">&gt;</a>
- 
-        </div>
-   <br>
- 
-<div style="text-align:center">
-  <span class="dot" onclick="currentSlide(1)"></span> 
-  <span class="dot" onclick="currentSlide(2)"></span> 
-  <span class="dot" onclick="currentSlide(3)"></span> 
-  <span class="dot" onclick="currentSlide(4)"></span> 
+<div class="mySlides fade">
+  <div class="numbertext">1 / 4</div>
+  <img src="foto1.jpg" style="width:100%">
+  <div class="text">Caption Text</div> 
 </div>
-   
+
+<div class="mySlides fade">
+  <div class="numbertext">2 / 4</div>
+  <img src="foto2.jpg" style="width:100%">
+  <div class="text">Caption Two</div>
+</div>
+
+<div class="mySlides fade">
+  <div class="numbertext">3 / 4</div>
+  <img src="foto3.jpg" style="width:100%">
+  <div class="text">Caption Three</div>
+</div>
+
+<div class="mySlides fade">
+  <div class="numbertext">4 / 4</div>
+  <img src="foto4.jpg" style="width:100%">
+  <div class="text">Caption Four</div>
+</div>
+
+
+</div>
+<br>
+
+<div style="text-align:center">
+  <span class="dot"></span> 
+  <span class="dot"></span> 
+  <span class="dot"></span> 
+  <span class="dot"></span> 
+</div>
+
+  
+   <br>
+   <script>
+  let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+
+</script>
 </body>
 </html>
